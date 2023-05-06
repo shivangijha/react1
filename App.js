a3d2256d27d08bec9af85da5eba12942a0ea3df8
System Administrator <root@Shivangis-MacBook-Air.local>
2023-05-06 21:00:20 +0530

    hide dist and parcel-cache

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
-// const heading = React.createElement("h1",{}, "React is on!!!");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement(
     "div", 
     {id:"parent"},
