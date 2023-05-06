4567c9a624cb42bdf2ed1591a561facbc988ae99
System Administrator <root@Shivangis-MacBook-Air.local>
2023-05-06 16:57:19 +0530

    remove extras

--- App.js
+++ App.js
@@ -18,23 +18,5 @@ const parent = React.createElement(
 
     
 );
-const headershit = React.createElement(
-    "div", 
-    {id:"parent"},
-      [React.createElement("div",{
-        id:"child1"}, 
-        [React.createElement("h1",{},"me so crazy first h1 tag"),
-         React.createElement("h2",{},"im first h2 tag")]),
-         React.createElement("div",{
-            id:"child2"}, 
-            [React.createElement("h1",{},"im first h1 tag"),
-             React.createElement("h2",{},"im first h2 tag")])]
-         
-
-    
-);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const header = ReactDOM.createRoot(document.getElementById("header"));
  root.render(parent);
- header.render(headershit);
